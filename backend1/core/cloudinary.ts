7fcd90b55ce1a7d175bd2ec0e4d42493d27f740a
Pynshik <60570875+Pynshik@users.noreply.github.com>
2021-02-15 13:04:19 +0300

    Update cloudinary.ts

--- backend1/core/cloudinary.ts
+++ backend1/core/cloudinary.ts
@@ -1,18 +1,14 @@
 import cloudinary from 'cloudinary';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-if(!process.env.CLOUDINARY_NAME) {
+if(!cloudinary) {
     throw new Error('Отсутствуют конфигурации для Cloudinary');
 }
 
 // @ts-ignore
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: 'dm3symelk',
+    api_key: '536929649312284',
+    api_secret: 'wZTC4D_6o0tKijlLAjKYNxeSe7A',
 });
 
 export default cloudinary;
-
